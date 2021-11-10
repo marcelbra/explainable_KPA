ed51838b8a5ab03f039a87a5bfbca1ef593e0065
marcelbra <marcelbraasch@gmail.com>
2021-11-10 18:55:57 +0100

    Added tokenizer + label

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset
+from datasets import load_dataset, DatasetDict
 from transformers import AutoTokenizer
 
 
@@ -11,19 +11,20 @@ tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 tokenizer(dataset["train"][0]["argument"])
 s = 0
 
+def get_label(dataset):
+    return [label["label"] for label in dataset["train"]]
 
-
+def concat_and_pad(x,y):
+    return x + y[1:] + [0] * (512 - len(x) - len(y) + 1)
 
 def tokenize_function(examples):
     argument_tokenized = tokenizer(examples["argument"])
     key_point_tokenized = tokenizer(examples["key_point"])
-    concat_tokenized = list(map(lambda x,y: x + y[1:] + [0]*(512-len(x)-len(y)+1),
-                                argument_tokenized["input_ids"], key_point_tokenized["input_ids"]))
-
-    #k = [1:]
-    s = 0
-    return argumet_tokenized
+    concat_tokenized = list(map(concat_and_pad, argument_tokenized["input_ids"], key_point_tokenized["input_ids"]))
+    return argument_tokenized
 
 tokenized_datasets = dataset.map(tokenize_function, batched=True)
+labels = get_label(dataset)
+
 
 s = 0
