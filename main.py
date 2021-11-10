1b687506d3e7d1ae7b0de7be731cc19ac2c130d4
marcelbra <marcelbraasch@gmail.com>
2021-11-10 18:04:31 +0100

    Modified few things

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ from datasets import load_dataset
 from transformers import AutoTokenizer
 
 
-# Load data
+# Load data and tokenizer
 dataset = load_dataset('csv', data_files=['ArgKP_dataset.csv'])
-# dataset["train"][0]
-
-# Load tokenizer
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
-# Tokenize dataset
+
+
+tokenizer(dataset["train"][0]["argument"])
+s = 0
+
+
+
+
 def tokenize_function(examples):
-    return tokenizer(examples["text"], padding="max_length", truncation=True)
+    examples['concat'] = list(map(lambda x,y: x + "|" + y, examples["argument"], examples["key_point"]))
+    concat_tokenized = tokenizer(examples["concat"])
+    argument_tokenized = tokenizer(examples["argument"])
+    key_point_tokenized = tokenizer(examples["key_point"])
+    s = 0
+    return argumet_tokenized
 
 tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
