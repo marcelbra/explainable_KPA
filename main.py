d981b318aeb94058736e1c79fc08f02633a8b2d0
marcelbra <marcelbraasch@gmail.com>
2021-11-10 18:38:58 +0100

    Added argument key-point concatenation

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ s = 0
 
 
 def tokenize_function(examples):
-    examples['concat'] = list(map(lambda x,y: x + "|" + y, examples["argument"], examples["key_point"]))
-    concat_tokenized = tokenizer(examples["concat"])
     argument_tokenized = tokenizer(examples["argument"])
     key_point_tokenized = tokenizer(examples["key_point"])
+    concat_tokenized = list(map(lambda x,y: x + y[1:] + [0]*(512-len(x)-len(y)+1),
+                                argument_tokenized["input_ids"], key_point_tokenized["input_ids"]))
+
+    #k = [1:]
     s = 0
     return argumet_tokenized
 
